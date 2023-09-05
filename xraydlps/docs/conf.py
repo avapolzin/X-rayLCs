c80733408096439f5919dc77928ba801d768967d
avapolzin <apolzin@uchicago.edu>
2023-09-05 13:02:59 -0500

    use underscores in theme extension?
    Why would they have inconsistent syntax????

--- xraydlps/docs/conf.py
+++ xraydlps/docs/conf.py
@@ -35,7 +35,7 @@ release = '1.0'
 extensions = [
 	'sphinx.ext.autodoc',
 	'sphinx.ext.napoleon',
-	'sphinx-rtd-theme'
+	'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
