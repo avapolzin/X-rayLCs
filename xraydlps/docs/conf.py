069c990e7cd5c2d5439063a65a348d736ce3d9b4
avapolzin <apolzin@uchicago.edu>
2023-09-05 14:08:22 -0500

    remove mock imports

--- xraydlps/docs/conf.py
+++ xraydlps/docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
-autodoc_mock_imports = ['numpy', 'matplotlib', 'pandas', 'scikit-learn', 'sklearn', 'astropy', 'scipy']
+# autodoc_mock_imports = ['numpy', 'matplotlib', 'pandas', 'scikit-learn', 'sklearn', 'astropy', 'scipy']
 
 autodoc_preserve_defaults = True
 
